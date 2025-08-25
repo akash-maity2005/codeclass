6d7f78fde094268303495efeb0db438ca17fb628
akash-maity2005 <maityakashkumar2005@gmail.com>
2025-08-25 19:43:47 +0530

    Initial commit

--- codeclass/3rdSemister/DSA/tower_of_hanoi.c
+++ codeclass/3rdSemister/DSA/tower_of_hanoi.c
@@ -1,16 +1,18 @@
-#include<stdio.h>
-void tower_of_hanoi(int n,int source,int inter,int dest){
-	if(n==0){
-		return 0;
-	}
-	tower_of_hanoi(n-1,source,inter,dest);
-	printf("Move %d to %d \n",source,dest);
-	tower_of_hanoi(n-1,inter,source,dest);
+#include <stdio.h>
+
+void tower_of_hanoi(int n, int source, int inter, int dest) {
+    if (n == 0) {
+        return;  
+    }
+ tower_of_hanoi(n - 1, source, dest, inter);
+    printf("Move disk %d from %d to %d\n", n, source, dest);
+    tower_of_hanoi(n - 1, inter, source, dest);
 }
-int main(){
-int n;
-printf("Enter the number of disk:\n");
-scanf("%d",&n);
-tower_of_hanoi(n,1,2,3);
-return 0;
+
+int main() {
+    int n;
+    printf("Enter the number of disks:\n");
+    scanf("%d", &n);
+     tower_of_hanoi(n, 1, 2, 3); 
+    return 0;
 }
